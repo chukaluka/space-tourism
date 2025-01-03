61c35404d1995611a287ac0521764fc67aa0e0df
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2025-01-03 12:57:08 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'crew', component: CrewComponent },
+  { path: 'technology', component: TechnologyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
